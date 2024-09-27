71b0d24b729d389251a330c555edfceea4f42ba5
hyerimhan <hnhyerim@gmail.com>
2024-09-27 20:25:20 +0900

    test

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,7 +4,13 @@ import { getDatabase } from 'firebase/database';
 // TODO: https://firebase.google.com/docs/web/setup?hl=ko
 // 가이드에 따라 firebase config를 설정합니다.
 const firebaseConfig = {
-
+  apiKey: 'AIzaSyDt9Mk73bNje6UgcobUZH-oXhDgxm9vvrU',
+  authDomain: 'wedding-invitation-a1437.firebaseapp.com',
+  databaseURL: 'https://wedding-invitation-a1437-default-rtdb.firebaseio.com',
+  projectId: 'wedding-invitation-a1437',
+  storageBucket: 'wedding-invitation-a1437.appspot.com',
+  messagingSenderId: '712106295641',
+  appId: '1:712106295641:web:f2661a595657be9701cc48',
 };
 
 export const firebaseApp = initializeApp(firebaseConfig);
